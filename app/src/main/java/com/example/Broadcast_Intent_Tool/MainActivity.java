dc1f3b94f2f703b23e0722bfd905e6d42b5c2f87
DuaneOne <duanethorpe@yahoo.com>
2017-02-26 19:39:37 -0500

    compatible with Mooshimeter-AndroidApp-34beta-BcastIntent

--- app/src/main/java/com/example/Broadcast_Intent_Tool/MainActivity.java
+++ app/src/main/java/com/example/Broadcast_Intent_Tool/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.Broadcast_Intent_Tool;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
-/* Broacast intent sender and receiver tool to help debug Mooshimeter send broadcast
-and/or Tasker receiver. This was modified slightly from a Tutorials point example.
+
+/* Broadcast intent sender and receiver tool to help debug Mooshimeter send broadcast
+and/or Tasker receiver. This was modified from a Tutorials point example.
 http://www.tutorialspoint.com/android/android_broadcast_receivers.htm*/
 
 public class MainActivity extends Activity {
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyReceiver.mainActivity = this;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
         setContentView(R.layout.activity_main);
     }
 
@@ -24,12 +32,19 @@ public class MainActivity extends Activity {
         return true;
     }
 
-    // broadcast a custom intent.
+    public void displayReceivedValues(String inString, Context context){
+        TextView tv = (TextView)findViewById(R.id.textView1);
+        tv.setText(inString);
+    }
+
+
+    // broadcast a custom intent in the same format that the mooshimeter app would except
+    // send only ch1 value, just for testing
     public void broadcastIntent(View view){
         Intent intent = new Intent();
-        //intent.setAction("com.tutorialspoint.CUSTOM_INTENT");
-        intent.setAction("com.mooshim.mooshimeter.CUSTOM_INTENT");
-        intent.putExtra("val", "3.14159");
+        intent.setAction("com.mooshim.mooshimeter.CH");
+        intent.putExtra("value1", Float.valueOf("3.14159") );
         sendBroadcast(intent);
     }
+
 }
--- app/src/main/java/com/example/Broadcast_Intent_Tool/MyReceiver.java
+++ app/src/main/java/com/example/Broadcast_Intent_Tool/MyReceiver.java
@@ -6,11 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+
 public class MyReceiver extends BroadcastReceiver {
+
+    public static MainActivity mainActivity;
+    private static int count = 0;
     @Override
+
+
     public void onReceive(Context context, Intent intent) {
         Bundle RecExtras = intent.getExtras() ;
-        String key1 = RecExtras.getString("val");
-        Toast.makeText(context, "MyReceiver.  " + key1, Toast.LENGTH_LONG).show();
+        // values are Floats , units are Strings
+        String ch1v = String.valueOf( RecExtras.getFloat("value1") );
+        String ch2v = String.valueOf( RecExtras.getFloat("value2") );
+        String ch3v = String.valueOf( RecExtras.getFloat("value3") );
+        String bat  = String.valueOf( RecExtras.getFloat("moobat") );
+
+        String ch1u =  RecExtras.getString("units1") ;
+        String ch2u =  RecExtras.getString("units2") ;
+        String ch3u =  RecExtras.getString("units3") ;
+
+        // \r\n puts next string on a new line
+        String all = "CH1: " + ch1v + " " + ch1u + "\r\n" +
+                     "CH2: " + ch2v + " " + ch2u + "\r\n" +
+                     "CH3: " + ch3v + " " + ch3u + "\r\n" +
+                     "BAT: " + bat;
+
+        //display string in a existing text box
+        mainActivity.displayReceivedValues(all, context);
+
+        // new intents come quickly and overwhelm the popup, so
+        // popup every nth occurrence
+        count = count+1;
+        if (count>= 9) {
+            Toast.makeText(context, all, Toast.LENGTH_SHORT).show();
+            count = 0;
+        }
+
     }
 }
